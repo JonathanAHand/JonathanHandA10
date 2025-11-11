77a9f9bd0b24a9201bd66b890120b791e414a7b0
Jonathan A Hand <jhand1@my.westga.edu>
2025-11-11 16:09:10 -0500

    Initial commit - Starter project setup for Assignment 10

---
+++ Assignment10/Model/Person/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment10.Model.Person
+{
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public int Id { get; set; }
+        
+        public Address.Address HomeAddress { get; set; }
+
+        protected Person()
+        {
+        }
+
+        public Person(string firstName, string lastName) : this(firstName, lastName, 0, null)
+        {
+        }
+
+        public Person(string firstName, string lastName, int id) : this(firstName, lastName, id, null)
+        {
+        }
+
+        public Person(string firstName, string lastName, int id, Address.Address homeAddress)
+        {
+            FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+            LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+            Id = id;
+            HomeAddress = homeAddress;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} : ID - {Id}";
+        }
+    }
+}
---
+++ Assignment10/Model/Person/Student.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment10.Model.Person
+{
+    public class Student : Person
+    {
+        private const string Undeclared = "Undeclared";
+
+        public string Major { get; set; }
+
+        public string Minor { get; set; }
+
+        private Student()
+        {
+
+        }
+
+        public Student(string firstName, string lastName) : this(firstName, lastName, 0, null, string.Empty)
+        {
+        }
+
+        public Student(string firstName, string lastName, int id) : this(firstName, lastName, id, null, string.Empty)
+        {
+        }
+
+        public Student(string firstName, string lastName, int id, Address.Address homeAddress) : this(firstName, lastName, id, homeAddress, string.Empty)
+        {
+        }
+
+        public Student(string firstName, string lastName, int id, Address.Address homeAddress, string major) : base(firstName, lastName, id, homeAddress)
+        {
+            Major = major ?? throw new ArgumentNullException(nameof(major));
+            Minor = Undeclared;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"; Major - {Major}";
+        }
+    }
+}
