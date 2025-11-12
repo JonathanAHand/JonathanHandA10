18a32be9bded484e873fd0b7c1fc6ec3ed3d11f9
Jonathan A Hand <jhand1@my.westga.edu>
2025-11-11 23:35:59 -0500

    Implemented Details property in Student class to complete Person abstraction

--- Assignment10/Model/Person/Student.cs
+++ Assignment10/Model/Person/Student.cs
@@ -4,13 +4,48 @@
     {
         private const string Undeclared = "Undeclared";
 
-        public string Major { get; set; }
+        private string _major;
+        private string _minor;
 
-        public string Minor { get; set; }
+        public string Major
+        {
+            get => _major;
+            set
+            {
+                if (value is null)
+                {
+                    throw new ArgumentNullException(nameof(value), "Major cannot be null.");
+                }
+
+                _major = value == string.Empty ? Undeclared : value;
+            }
+        }
+
+        public string Minor
+        {
+            get => _minor;
+            set
+            {
+                if (value is null)
+                {
+                    throw new ArgumentNullException(nameof(value), "Minor cannot be null.");
+                }
+
+                _minor = value == string.Empty ? Undeclared : value;
+            }
+        }
+
+        public override string Details
+        {
+            get
+            {
+                string addressInfo = HomeAddress != null ? HomeAddress.ToString() : "No Address";
+                return $"Name: {FirstName} {LastName}, ID: {Id}, Major: {Major}, Minor: {Minor}, Address: {addressInfo}";
+            }
+        }
 
         private Student()
         {
-
         }
 
         public Student(string firstName, string lastName) : this(firstName, lastName, 0, null, string.Empty)
@@ -25,7 +60,8 @@
         {
         }
 
-        public Student(string firstName, string lastName, int id, Address.Address homeAddress, string major) : base(firstName, lastName, id, homeAddress)
+        public Student(string firstName, string lastName, int id, Address.Address homeAddress, string major)
+            : base(firstName, lastName, id, homeAddress)
         {
             Major = major ?? throw new ArgumentNullException(nameof(major));
             Minor = Undeclared;
